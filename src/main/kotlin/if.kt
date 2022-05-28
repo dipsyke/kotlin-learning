b59e69eef47563b0f040f1b54e1b0c47b2b6f644
dipsyke <pogyornika@gmail.com>
2022-05-28 22:16:36 +0200

    added example for if

---
+++ src/main/kotlin/if.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val logikaiValami: Boolean = 1 == 1
+
+    if(logikaiValami /*condition */) { //fejléc
+        // then törzs
+        println("Ez a feltétel igaz!")
+    } else{
+        // else törzs
+        println("Ez a feltétel hamis :'(")
+    }
+}
