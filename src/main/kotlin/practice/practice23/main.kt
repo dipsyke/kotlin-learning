f37bc9702b95da38510b68be6aa1eb04db5290b8
dipsyke <pogyornika@gmail.com>
2022-12-07 02:30:42 +0100

    updated practice23

--- src/main/kotlin/practice/practice23/main.kt
+++ src/main/kotlin/practice/practice23/main.kt
@@ -6,19 +6,52 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+class ColorComponentMultipliers (val redComponent: Double, val greenComponent: Double, val blueComponent: Double) {}
 
 fun main() {
+
+    //var redComponent: Double = 0.0
+//    var greenComponent: Double = 0.0
+//    var blueComponent: Double = 0.0
+
     print("Add meg a fájl nevét: ")
     val inputFileName = readLine()!!
     print("Add meg a fájl kiterjesztését: ")
     val inputFileExtension = readLine()!!
-    println("Milyen színű képet szeretnél? Add meg a komponensek szorzóit!")
-    print("Add meg a piros szorzóját: ")
-    val redComponent = readLine()!!.toDouble()
-    print("Add meg a zöld szorzóját: ")
-    val greenComponent = readLine()!!.toDouble()
-    print("Add meg a kék szorzóját: ")
-    val blueComponent = readLine()!!.toDouble()
+    //println("Milyen színű képet szeretnél? Add meg a komponensek szorzóit!")
+    println("Milyen színű képet szeretnél? (neon, greyscale, custom)")
+    val colorOption = readLine()!!
+    var colorComponentMultipliers = ColorComponentMultipliers(0.0,0.0,0.0)
+
+    when (colorOption) {
+        "neon" -> {
+            print("Milyen neon színt szeretnél? (red, green, blue)")
+            val neonColorOption = readLine()!!
+            if (neonColorOption == "red") {
+                colorComponentMultipliers = ColorComponentMultipliers(2.55, 0.49, 0.49)
+            }
+            if (neonColorOption == "green") {
+                colorComponentMultipliers = ColorComponentMultipliers(0.244, 1.0, 0.078)
+
+            }
+            if (neonColorOption == "blue") {
+                colorComponentMultipliers = ColorComponentMultipliers(0.31, 0.81, 2.55)
+            }
+        }
+
+        "greyscale" -> {var colorComponentGreyscale = ColorComponentMultipliers(1.0, 1.0, 1.0)}
+
+//        "custom" -> {
+//            print("Add meg a piros szorzóját: ")
+//            redComponent = readLine()!!.toDouble()
+//            print("Add meg a zöld szorzóját: ")
+//            greenComponent = readLine()!!.toDouble()
+//            print("Add meg a kék szorzóját: ")
+//            blueComponent = readLine()!!.toDouble()
+//        }
+    else ->         println("This is not a valid option")
+    }
+
     val bufferedImage: BufferedImage = ImageIO.read(File("C:\\tmp\\$inputFileName.$inputFileExtension"))
 
     println("a kép szélessége: ${bufferedImage.width}")
@@ -50,9 +83,9 @@ fun main() {
             average += originalPixelMap[y][x].blue
             average = average / 3
             //val grayscale = Color(average, average, average)
-            val minRed = minOf(255, (redComponent * average).toInt())
-            val minGreen = minOf(255, (greenComponent * average).toInt())
-            val minBlue = minOf(255, (blueComponent * average).toInt())
+            val minRed = minOf(255, (colorComponentMultipliers.redComponent * average).toInt())
+            val minGreen = minOf(255, (colorComponentMultipliers.greenComponent * average).toInt())
+            val minBlue = minOf(255, (colorComponentMultipliers.blueComponent * average).toInt())
             val grayscale = Color(minRed, minGreen, minBlue)
             greyscalePixelMap[y][x] = grayscale
 
